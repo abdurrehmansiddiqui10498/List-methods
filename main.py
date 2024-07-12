5305461407039ab93317d51da46f5969dbf3e5fe
Abdur Rehman Siddiqui <abisbah2011@gmail.com>
2024-07-12 14:03:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Reverse what you do daily
+print("Enter what you do daily(After having breakfast)")
+list = list(map(str,input(). split(",")))
+print("This is your regular list:")
+for lists in list:
+ print(lists)
+print("Now your reversed list is:")
+b = (list[::-1])
+for c in b:
+  print(c)
